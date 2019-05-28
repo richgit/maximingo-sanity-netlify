5b1ed5cae2305abf20f71ce0ee4da80ead3d2e9c
Richard Ware <richard.ware@datacom.co.nz>
2019-05-29 10:28:08 +1200

    article on index page

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -8,6 +8,7 @@ import {
 import Container from '../components/container'
 import GraphQLErrorList from '../components/graphql-error-list'
 import ProjectPreviewGrid from '../components/project-preview-grid'
+import ArticlePreviewGrid from '../components/article-preview-grid'
 import SEO from '../components/seo'
 import Layout from '../containers/layout'
 
@@ -18,6 +19,40 @@ export const query = graphql`
       description
       keywords
     }
+    articles: allSanityArticle(
+      limit: 6
+      sort: {fields: [publishedAt], order: DESC}
+    ) {
+      edges {
+        node {
+          id
+          mainImage {
+            crop {
+              _key
+              _type
+              top
+              bottom
+              left
+              right
+            }
+            hotspot {
+              _key
+              _type
+              x
+              y
+              height
+              width
+            }
+            asset {
+              _id
+            }
+            alt
+          }
+          title
+          _rawExcerpt
+        }
+      }
+     }
     projects: allSanityProject(
       limit: 6
       sort: {fields: [publishedAt], order: DESC}
@@ -50,6 +85,7 @@ export const query = graphql`
           }
           title
           _rawExcerpt
+          articleUrl
           slug {
             current
           }
@@ -77,6 +113,12 @@ const IndexPage = props => {
       .filter(filterOutDocsPublishedInTheFuture)
     : []
 
+  const articleNodes = (data || {}).articles
+    ? mapEdgesToNodes(data.articles)
+      .filter(filterOutDocsPublishedInTheFuture)
+    : []
+
+
   if (!site) {
     throw new Error(
       'Missing "Site settings". Open the studio at http://localhost:3333 and add some content to "Site settings" and restart the development server.'
@@ -87,11 +129,18 @@ const IndexPage = props => {
     <Layout>
       <SEO title={site.title} description={site.description} keywords={site.keywords} />
       <Container>
-        <h1 hidden>Welcome to {site.title}</h1>
+        <h1 hidden>{site.title}</h1>
         {projectNodes && (
           <ProjectPreviewGrid
-            title='Interesting articles'
+            title='Latest projects'
             nodes={projectNodes}
+            browseMoreHref='/archive/'
+          />
+        )}
+        {articleNodes && (
+          <ArticlePreviewGrid
+            title='Latest articles'
+            nodes={articleNodes}
             browseMoreHref='/archive/'
           />
         )}
